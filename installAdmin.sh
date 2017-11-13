da1cd14ad8cb3f71927173a333be8d9e7d1808f6
harokb <harokb@gmail.com>
2017-11-13 16:33:59 +0100

    Update installAdmin.sh

--- installAdmin.sh
+++ installAdmin.sh
@@ -9,6 +9,7 @@ PASSWORD=ceph
 wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 echo deb https://download.ceph.com/debian-luminous/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
 sudo apt update
+sudo apt install apt-transport-https
 sudo apt install ceph-deploy
 
 # Create user
