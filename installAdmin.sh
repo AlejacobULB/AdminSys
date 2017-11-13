246bbd2e9ba34a3aed973312de2ae595076c466e
harokb <harokb@gmail.com>
2017-11-13 16:18:54 +0100

    Update and rename install.sh to installAdmin.sh

---
+++ installAdmin.sh
@@ -0,0 +1,48 @@
+SERVER=$1
+SERVER_USERNAME=admin
+CEPH_USERNAME=cephUser
+PASSWORD=ceph
+
+# Install ceph-deploy
+wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
+echo deb https://download.ceph.com/debian-luminous/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
+sudo apt update
+sudo apt install ceph-deploy
+
+# Install NTP (to avoid clock skew)
+# sudo apt install ntp
+
+
+# Install SSH server
+sudo apt install openssh-server
+
+# Create user
+ssh $SERVER_USERNAME@$SERVER
+sudo useradd -d /home/$CEPH_USERNAME -m $CEPH_USERNAME
+echo "$PASSWORD" | passwd "$CEPH_USERNAME" --stdin
+
+# Add sudo privileges to newly created user
+echo "$CEPH_USERNAME ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$CEPH_USERNAME
+sudo chmod 0440 /etc/sudoers.d/$CEPH_USERNAME
+
+# Create SSH keys and copy them to the nodes
+cat /dev/zero | ssh-keygen -q -N ""
+ssh-copy-id $SERVER_USERNAME@server2
+ssh-copy-id $SERVER_USERNAME@server3
+ssh-copy-id $SERVER_USERNAME@server4
+
+# Modify the ~/.ssh/config file of your ceph-deploy admin node so that ceph-deploy can 
+# log in to Ceph nodes as the user you created without requiring you to specify --username {username} each 
+# time you execute ceph-deploy. 
+# This has the added benefit of streamlining ssh and scp usage
+cat >>~/.ssh/config <<EOL
+Host server2
+   Hostname server2
+   User $CEPH_USERNAME
+Host server3
+   Hostname server3
+   User $CEPH_USERNAME
+Host server4
+   Hostname server4
+   User $CEPH_USERNAME
+EOL
