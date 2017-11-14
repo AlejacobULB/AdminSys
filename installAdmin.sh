5a4452146bd2cff5caaa31dbc496425b52866329
David <David.Engelman@ulb.ac.be>
2017-11-14 13:00:07 +0100

    update

--- installAdmin.sh
+++ installAdmin.sh
@@ -1,37 +1,30 @@
 SERVER=$1
-SERVER_USERNAME=admin
+SERVER_USERNAME=admin-node
 CEPH_USERNAME=cephUser
 PASSWORD=ceph
 
+# ssh -p 3022 $SERVER_USERNAME@127.0.0.1
 
-# Connect to admin server via ssh and install ceph-deploy
+echo $'\n ===>Setting up APT<=== \n'
+sudo apt install apt-transport-https
 
-ssh -p 3022 $SERVER_USERNAME.-.$SERVER@localhost << EOF
+# Install ceph-deploy
+wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
+echo deb https://download.ceph.com/debian-luminous/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
 
-	echo $'\n ===>Setting up APT<=== \n'
-	sudo apt install apt-transport-https
+echo $'\n ===>apt-update<=== \n'
+sudo apt update
 
-	# Install ceph-deploy
-	wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
-	echo deb https://download.ceph.com/debian-luminous/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
+echo $'\n ===>Installing ceph-deploy<=== \n'
+sudo apt install ceph-deploy -y
 
-	echo $'\n ===>apt-update<=== \n'
-	sudo apt update
+# Create user
+echo $'\n ===>Creating Ceph user<=== \n'
+sudo useradd -d /home/$CEPH_USERNAME -m $CEPH_USERNAME
+echo "$CEPH_USERNAME:$PASSWORD" | sudo /usr/sbin/chpasswd
 
-	echo $'\n ===>Installing ceph-deploy<=== \n'
-	sudo apt install ceph-deploy -y
-
-	# Create user
-	echo $'\n ===>Creating Ceph user<=== \n'
-	sudo useradd -d /home/$CEPH_USERNAME -m $CEPH_USERNAME
-	echo "$CEPH_USERNAME:$PASSWORD" | sudo /usr/sbin/chpasswd
-
-	echo $'\n ==>Adding sudo privileges to newly created user<=== \n'
-	# Add sudo privileges to newly created user
-	echo "$CEPH_USERNAME ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$CEPH_USERNAME
-	sudo chmod 0440 /etc/sudoers.d/$CEPH_USERNAME
-
-
-	EOF
-
+echo $'\n ==>Adding sudo privileges to newly created user<=== \n'
+# Add sudo privileges to newly created user
+echo "$CEPH_USERNAME ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$CEPH_USERNAME
+sudo chmod 0440 /etc/sudoers.d/$CEPH_USERNAME
 
