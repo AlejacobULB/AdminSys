7d72ae40e000beb34d49d14a55228e749fe4b4f6
David <David.Engelman@ulb.ac.be>
2017-11-14 11:34:18 +0100

    update installAdmin and create installNodes

---
+++ installNodes.sh
@@ -0,0 +1,26 @@
+
+# This script setup all ceph nodes
+
+
+CEPH_USERNAME=cephUser
+PASSWORD=ceph
+
+for serverToConfig in $@
+do
+
+	ssh -p 3022 admin-.serverToConfig@localhost << EOF
+
+	# Create user
+	echo $'\n ===>Creating Ceph user<=== \n'
+	sudo useradd -d /home/$CEPH_USERNAME -m $CEPH_USERNAME
+	echo "$CEPH_USERNAME:$PASSWORD" | sudo /usr/sbin/chpasswd
+
+	echo $'\n ==>Adding sudo privileges to newly created user<=== \n'
+	# Add sudo privileges to newly created user
+	echo "$CEPH_USERNAME ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$CEPH_USERNAME
+	sudo chmod 0440 /etc/sudoers.d/$CEPH_USERNAME
+
+	EOF
+
+done
+
