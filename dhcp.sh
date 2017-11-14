6877c21f1bdfcfac4ed8d215d43afa81d12f3997
Engelman David <daengelm@ulb.ac.be>
2017-11-14 15:43:33 +0100

    Delete dhcp.sh

--- dhcp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Virtual Box DHCP server: shutting down"
-VBoxManage dhcpserver remove --netname intnet
-echo "Virtual Box DHCP server: starting up"
-VBoxManage dhcpserver add --netname intnet --ip 10.0.0.1 --netmask 255.255.255.0 --enable --lowerip 10.0.0.100 --upperip 10.0.0.200 
-
