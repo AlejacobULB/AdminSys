efab96ec903672af52fdd5d98ff2c89a79717702
harokb <harokb@gmail.com>
2017-11-17 15:24:45 +0100

    Improved documentation

--- scripts/runInstallNodes.sh
+++ scripts/runInstallNodes.sh
@@ -8,6 +8,9 @@ port=3022
 for ((i=0; i<NUMBER_OF_NODES; i++));
 do
 	echo $'\n ===>Connecting to port '$port'<=== \n' 
+	# For this to run correctly, we needed to change sudoers beforehand so
+	# that sudo'ing as admin doesn't require a password
+	# TODO: find a better solution
 	cat installNodes.sh | ssh -p $port admin@localhost
 	((port++))
 done
