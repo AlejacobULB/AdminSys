b6ee25b7e8af08cad18547e0cf71974587605c0e
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-11-21 11:00:25 +0100

    update

--- scripts/createCluster.sh
+++ scripts/createCluster.sh
@@ -10,15 +10,11 @@ echo "\nInstall Ceph packages on nodes\n"
 ceph-deploy install node1 node2 node3
 
 echo "\nDeploy the initial monitor(s) and gather the keys (pas trop compris ce que ca fait)\n"
-ceph-deploy --overwrite-conf mon create-initial 
+ceph-deploy --overwrite-conf mon create-initial
 
 echo "\nCopy the configuration file and admin key to your admin node and your Ceph Nodes\n"
 ceph-deploy admin node1 node2 node3
 
-#Deploy a manager daemon
-# ceph-deploy mgr create  node2 node3
-
-#DONE CREATE /DEV/VDB ON ALL
 echo "\nAdd OSD's on nodes\n"
 ceph-deploy osd create node1:sdb node2:sdb node3:sdb
 
