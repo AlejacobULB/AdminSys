eacd422b68138b7650d3d5a8e151d28d0d36ef20
Alexandre Jacobs <jacalex@live.be>
2017-11-22 20:15:08 +0100

    add command to install ntp server

--- scripts/installNodes.sh
+++ scripts/installNodes.sh
@@ -15,3 +15,6 @@ echo $'\n ==>Adding sudo privileges to newly created user<=== \n'
 # Add sudo privileges to newly created user
 echo "$CEPH_USERNAME ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$CEPH_USERNAME
 sudo chmod 0440 /etc/sudoers.d/$CEPH_USERNAME
+
+#Install NTP Server
+sudo apt -y install ntp
